c9fa2a1ca4e88b53151e1e151e8a965b53a9a2b2
Colin Bellino <bellino.colin@gmail.com>
2018-09-13 01:39:10 +0200

    🔧 Switch to create-react-app "vanilla" with rewired

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const {
+  rewireWebpack: rewireTypescript,
+  rewireJest: rewireTypescriptJest,
+  rewireTSLint,
+} = require("react-app-rewire-typescript-babel-preset");
+
+module.exports = {
+  webpack: function(config, env) {
+    config = rewireTypescript(config);
+
+    config = rewireTSLint(config, { typeCheck: true });
+
+    return config;
+  },
+  jest: function(config) {
+    return rewireTypescriptJest(config);
+  },
+};
